3ea8607346a128fdf59e030d24693d2a6d53bcf2
seunggonhyeon <hyunsg5638@gmail.com>
2024-11-19 03:23:12 +0900

    fix : link error

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -61,7 +61,7 @@ public class ArticleController {
     }
 
     // 데이터 목록 조회
-    @GetMapping("/articles/")
+    @GetMapping("/articles")
     public String index(Model model) {
         // 1. 모든 데이터 가져오기
         ArrayList<Article> articleEntityList = articleRepository.findAll();
