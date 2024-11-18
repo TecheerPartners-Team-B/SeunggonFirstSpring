a6392a429b7278be3c01947556772732b36a1e74
seunggonhyeon <hyunsg5638@gmail.com>
2024-11-19 02:51:19 +0900

    feat: ArticleList Read (GET)

--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -3,7 +3,11 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
     // 기본 동작을 정의할 필요없이 CrudRepository에서 상속받아 사용할 수 있다.
 
+    @Override
+    ArrayList<Article> findAll();
 }
